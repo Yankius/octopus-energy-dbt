fb0accf56a84df931c6c76876ecd042a12c6788b
Kiril Tasev <yankee_cannibal@yahoo.com>
2026-03-31 00:54:30 +0100

    fix: universal bigquery syntax for missing intervals test

--- tests/no_missing_intervals.sql
+++ tests/no_missing_intervals.sql
@@ -1,17 +1,26 @@
+/*
+  This test ensures there are no gaps in the 30-minute consumption data.
+  It generates a complete series of timestamps between the min and max 
+  found in the fact table and checks if any are missing.
+*/
+
 with series as (
 
-    select *
-    from generate_series(
-        (select min(interval_start_utc) from {{ ref('fct_energy_cost') }}),
-        (select max(interval_start_utc) from {{ ref('fct_energy_cost') }}),
-        interval '30 minutes'
-    ) as t(interval_start_utc)
+    select interval_start_utc
+    from unnest(
+        generate_timestamp_array(
+            (select min(interval_start_utc) from {{ ref('fct_energy_cost') }}),
+            (select max(interval_start_utc) from {{ ref('fct_energy_cost') }}),
+            interval 30 minute
+        )
+    ) as interval_start_utc
 
 ),
 
 missing as (
 
-    select s.interval_start_utc
+    select 
+        s.interval_start_utc
     from series s
     left join {{ ref('fct_energy_cost') }} f
         on s.interval_start_utc = f.interval_start_utc
